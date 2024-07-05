f89aad39d619d1c69370693e522352ab28618b5a
aleksandre jachvadze <155845898+Aleksandre-jachvadze@users.noreply.github.com>
2024-07-05 16:28:48 +0400

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,142 +1,176 @@
-import { useRef, useState } from 'react'
-import Notification from './Notification'
-
-import './scss/style.scss'
-
-const notifications = [
-  {
-    id: 1,
-    isRead: false,
-    user: {
-      name: 'Mark Webber',
-      img: 'assets/images/avatar-mark-webber.webp',
-    },
-    text: 'react to your recent post',
-    payload: { type: 'link', text: 'My first tournament today!', url: '#' },
-    time: '1m ago',
-  },
-  {
-    id: 2,
-    isRead: false,
-    user: {
-      name: 'Angela Gray',
-      img: 'assets/images/avatar-angela-gray.webp',
-    },
-    text: 'followed you',
-    payload: { type: 'message' },
-    time: '5m ago',
-  },
-  {
-    id: 3,
-    isRead: false,
-    user: {
-      name: 'Jacob Thompson',
-      img: 'assets/images/avatar-jacob-thompson.webp',
-    },
-    text: 'has joined your group',
-    payload: { type: 'link', text: 'Chess Club', url: '#', important: true },
-    time: '1 day ago',
-  },
-  {
-    id: 4,
-    isRead: true,
-    user: {
-      name: 'Rizky Hasanuddin',
-      img: 'assets/images/avatar-rizky-hasanuddin.webp',
-    },
-    text: 'sent you a private message',
-    payload: {
-      type: 'msg',
-      msg: `Hello, thanks for setting up the Chess Club. I've been a member for a few weeks now and I'm already having lots of fun and improving my game.`,
-    },
-    time: '5 days ago',
-  },
-  {
-    id: 5,
-    isRead: true,
-    user: {
-      name: 'Kimberly Smith',
-      img: 'assets/images/avatar-kimberly-smith.webp',
-    },
-    text: 'commented on your picture',
-    payload: { type: 'img', url: 'assets/images/image-chess.webp' },
-    time: '1 week ago',
-  },
-  {
-    id: 6,
-    isRead: true,
-    user: {
-      name: 'Nathan Peterson',
-      img: 'assets/images/avatar-nathan-peterson.webp',
-    },
-    text: 'react to your recent post',
-    payload: {
-      type: 'link',
-      text: '5 end-game strategies to increase your win rate',
-      url: '#',
-    },
-    time: '2 weeks ago',
-  },
-  {
-    id: 7,
-    isRead: true,
-    user: {
-      name: 'Anna Kim',
-      img: 'assets/images/avatar-anna-kim.webp',
-    },
-    text: 'left the group',
-    payload: { type: 'link', text: 'Chess Club', url: '#', important: true },
-    time: '2 weeks ago',
-  },
-]
+import React from "react";
+import "./App.css";
+import markwebber from "./assets/avatar-mark-webber.webp";
+import angela from "./assets/avatar-angela-gray.webp";
+import jacob from "./assets/avatar-jacob-thompson.webp";
+import rizky from "./assets/avatar-rizky-hasanuddin.webp";
+import kimberly from "./assets/avatar-kimberly-smith.webp";
+import nathan from "./assets/avatar-nathan-peterson.webp";
+import anna from "./assets/avatar-anna-kim.webp";
+import myPic from "./assets/image-chess.webp";
 
 function App() {
-  const [state, setState] = useState(notifications)
-  const notificationEl = useRef()
-
-  function handleReadAll() {
-    const numOfNotification = state.filter(
-      (notificaton) => !notificaton.isRead
-    ).length
-
-    if (numOfNotification > 0) {
-      notificationEl.current.classList.add('animate')
-    }
-
-    setTimeout(() => {
-      notificationEl.current.classList.remove('animate')
-
-      setState((prev) =>
-        prev.map((notif) => {
-          if (!notif.isRead) {
-            notif.isRead = true
-          }
-
-          return notif
-        })
-      )
-    }, 222)
-  }
-
   return (
-    <>
-      <header className="header | flex">
-        <h1>Notifications</h1>
-        <span className="badge badge-primary" ref={notificationEl}>
-          {state.filter((notificaton) => !notificaton.isRead).length}
-        </span>
-        <button className="btn btn-text" onClick={handleReadAll}>
+    <div className="App">
+      <div className="top-bar">
+        <div className="title">
+          <h2 className="title-text">Notifications</h2>
+          <h3 className="num" id="num">
+            0
+          </h3>
+        </div>
+        <a href="#" className="read" id="read">
           Mark all as read
-        </button>
-      </header>
+        </a>
+      </div>
 
-      <main>
-        <ul className="notification-list">
-          {state.map((n) => (
-            <Notification key={n.id} data={n} />
-          ))}
-        </ul>
-      </main>
-    </>
-  )
+      <div className="notifications">
+        {/* Single notification starts */}
+        <div className="single-box unread">
+          <div className="avatar">
+            <img src={markwebber} alt="Mark Webber" />
+          </div>
+          <div className="box-text">
+            <p className="notifi-text">
+              <a href="#" className="profile-name">
+                Mark Webber
+              </a>{" "}
+              reacted to your recent post{" "}
+              <a href="#" className="post">
+                My First tournament today!
+              </a>
+              <span className="dot"></span>
+            </p>
+            <p className="time">1m ago</p>
+          </div>
+        </div>
+        {/* Single notification end */}
+
+        {/* Single notification starts */}
+        <div className="single-box">
+          <div className="avatar">
+            <img src={angela} alt="Angela Gray" />
+          </div>
+          <div className="box-text">
+            <p className="notifi-text">
+              <a href="#" className="profile-name">
+                Angela Gray
+              </a>{" "}
+              followed you
+              <span className="dot"></span>
+            </p>
+            <p className="time">5m ago</p>
+          </div>
+        </div>
+        {/* Single notification end */}
+
+        {/* Single notification starts */}
+        <div className="single-box">
+          <div className="avatar">
+            <img src={jacob} alt="Jacob Thompson" />
+          </div>
+          <div className="box-text">
+            <p className="notifi-text">
+              <a href="#" className="profile-name">
+                Jacob Thompson
+              </a>{" "}
+              has joined your group{" "}
+              <a href="#" className="post">
+                Chess Club
+              </a>
+              <span className="dot"></span>
+            </p>
+            <p className="time">1 day ago</p>
+          </div>
+        </div>
+        {/* Single notification end */}
+
+        {/* Single notification starts */}
+        <div className="single-box">
+          <div className="avatar">
+            <img src={rizky} alt="Rizky Hasanuddin" />
+          </div>
+          <div className="box-text">
+            <p className="notifi-text">
+              <a href="#" className="profile-name">
+                Rizky Hasanuddin
+              </a>{" "}
+              sent you a private message
+              <span className="dot"></span>
+            </p>
+            <p className="time">5 days ago</p>
+            <div className="private-text">
+              Hello, thanks for setting up the Chess Club. I've been a member
+              for a few weeks now and I'm already having lots of fun and
+              improving my game.
+            </div>
+          </div>
+        </div>
+        {/* Single notification end */}
+
+        {/* Single notification starts */}
+        <div className="single-box">
+          <div className="avatar">
+            <img src={kimberly} alt="Kimberly Smith" />
+          </div>
+          <div className="box-text">
+            <p className="notifi-text">
+              <a href="#" className="profile-name">
+                Kimberly Smith
+              </a>{" "}
+              commented on your picture
+              <span className="dot"></span>
+            </p>
+            <p className="time">1 week ago</p>
+          </div>
+          <div className="left-img">
+            <img src={myPic} alt="My Picture" />
+          </div>
+        </div>
+        {/* Single notification end */}
+
+        {/* Single notification starts */}
+        <div className="single-box">
+          <div className="avatar">
+            <img src={nathan} alt="Nathan Peterson" />
+          </div>
+          <div className="box-text">
+            <p className="notifi-text">
+              <a href="#" className="profile-name">
+                Nathan Peterson
+              </a>{" "}
+              reacted to your recent post{" "}
+              <a href="#" className="post">
+                5 end-game strategies to increase your win rate
+              </a>
+              <span className="dot"></span>
+            </p>
+            <p className="time">2 weeks ago</p>
+          </div>
+        </div>
+        {/* Single notification end */}
+
+        {/* Single notification starts */}
+        <div className="single-box">
+          <div className="avatar">
+            <img src={anna} alt="Anna Kim" />
+          </div>
+          <div className="box-text">
+            <p className="notifi-text">
+              <a href="#" className="profile-name">
+                Anna Kim
+              </a>{" "}
+              left the group{" "}
+              <a href="#" className="post">
+                Chess Club
+              </a>
+              <span className="dot"></span>
+            </p>
+            <p className="time">2 weeks ago</p>
+          </div>
+        </div>
+        {/* Single notification end */}
+      </div>
+    </div>
+  );
 }
